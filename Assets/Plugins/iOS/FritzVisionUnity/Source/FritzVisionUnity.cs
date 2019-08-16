28d9f60ff8a97991415421c2f7205298a8862404
Chris Kelly <ghop02@gmail.com>
2019-08-16 11:39:50 -0400

    Updating FritzVisionUnity to take camera input from unity arkit

--- Assets/Plugins/iOS/FritzVisionUnity/Source/FritzVisionUnity.cs
+++ Assets/Plugins/iOS/FritzVisionUnity/Source/FritzVisionUnity.cs
@@ -3,24 +3,18 @@ using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 
-public class FritzVisionUnity : MonoBehaviour
+public class FritzPoseManager
 {
-
-    protected static string debugPoseMessage = "[[[0,180.67385578155518,171.22436904907227,0.20715008676052094],[1,176.22251681983471,170.16234302520752,0.12665387988090515],[2,180.38571119308472,169.48479127883911,0.31717085838317871],[3,169.78597116470337,172.18780732154846,0.36716234683990479],[4,176.92858785390854,170.53509187698364,0.84875857830047607],[5,160.12279987335205,182.33350086212158,0.95805686712265015],[6,180.52317190170288,183.13732814788818,0.99377453327178955],[7,158.10193943977356,196.39807319641116,0.74261933565139771],[8,195.79326581954956,192.81923198699951,0.90857023000717163],[9,142.70438385009766,203.86272954940796,0.54645127058029175],[10,204.6772677898407,197.00697612762451,0.87599426507949829],[11,172.86505317687988,218.25026512145996,0.3790256679058075],[12,185.68683433532715,215.4071378707886,0.98059260845184326],[13,178.38080716133118,235.5998330116272,0.88386666774749756],[14,182.5034646987915,235.51912498474118,0.89248514175415039],[15,189.0296745300293,256.92245519161224,0.71080482006072998],[16,192.38013252615929,257.30943143367767,0.90547037124633789]]]";
     #region Declare external C interface
 
 #if UNITY_IOS && !UNITY_EDITOR
         
     [DllImport("__Internal")]
     private static extern string _configure();
-
-    [DllImport("__Internal")]
-    private static extern void _startCamera();
-
-    [DllImport("__Internal")]
-    private static extern void _stopCamera();
 
     [DllImport("__Internal")]
     private static extern void _setMinPartThreshold(double threshold);
@@ -32,8 +26,7 @@ public class FritzVisionUnity : MonoBehaviour
     private static extern void _setNumPoses(int poses);
 
     [DllImport("__Internal")]
-    private static extern string _getLatestPose();
-
+    private static extern string _processPose(IntPtr buffer);
 
 #endif
 
@@ -50,23 +43,6 @@ public class FritzVisionUnity : MonoBehaviour
 #endif
     }
 
-    public static void StartCamera()
-    {
-#if UNITY_IOS && !UNITY_EDITOR
-        _startCamera();
-#else
-        
-#endif
-    }
-
-    public static void StopCamera()
-    {
-#if UNITY_IOS && !UNITY_EDITOR
-        _stopCamera();
-#else
-#endif
-    }
-
     public static void SetNumPoses(int numPoses)
     {
 #if UNITY_IOS && !UNITY_EDITOR
@@ -75,13 +51,17 @@ public class FritzVisionUnity : MonoBehaviour
 #endif
     }
 
-    public static List<FritzPose> GetLatestPose()
+    public static List<FritzPose> ProcessPose(IntPtr buffer)
     {
-        string message;
+        var message = "";
+        if (buffer == IntPtr.Zero)
+        {
+            Debug.LogError("buffer is NULL!");
+            return null;
+        }
+
 #if UNITY_IOS && !UNITY_EDITOR
-        message = _getLatestPose();
-#else
-        message = debugPoseMessage;
+           message = _processPose(buffer);
 #endif
         List<List<List<float>>> poses = JsonConvert.DeserializeObject<List<List<List<float>>>>(message);
         if (poses == null)
@@ -98,6 +78,30 @@ public class FritzVisionUnity : MonoBehaviour
 
     #endregion
 
+
+
+}
+
+
+
+public class FritzVisionUnity : MonoBehaviour
+{
+
+    [SerializeField]
+    [Tooltip("The ARCameraManager which will produce frame events.")]
+    ARCameraManager m_CameraManager;
+
+    public ARCameraManager cameraManager
+    {
+        get => m_CameraManager;
+        set => m_CameraManager = value;
+    }
+
+    [SerializeField]
+    Camera m_Cam;
+
+    protected static string debugPoseMessage = "[[[0,180.67385578155518,171.22436904907227,0.20715008676052094],[1,176.22251681983471,170.16234302520752,0.12665387988090515],[2,180.38571119308472,169.48479127883911,0.31717085838317871],[3,169.78597116470337,172.18780732154846,0.36716234683990479],[4,176.92858785390854,170.53509187698364,0.84875857830047607],[5,160.12279987335205,182.33350086212158,0.95805686712265015],[6,180.52317190170288,183.13732814788818,0.99377453327178955],[7,158.10193943977356,196.39807319641116,0.74261933565139771],[8,195.79326581954956,192.81923198699951,0.90857023000717163],[9,142.70438385009766,203.86272954940796,0.54645127058029175],[10,204.6772677898407,197.00697612762451,0.87599426507949829],[11,172.86505317687988,218.25026512145996,0.3790256679058075],[12,185.68683433532715,215.4071378707886,0.98059260845184326],[13,178.38080716133118,235.5998330116272,0.88386666774749756],[14,182.5034646987915,235.51912498474118,0.89248514175415039],[15,189.0296745300293,256.92245519161224,0.71080482006072998],[16,192.38013252615929,257.30943143367767,0.90547037124633789]]]";
+
     #region Singleton implementation
 
     private static FritzVisionUnity _instance;
@@ -108,7 +112,7 @@ public class FritzVisionUnity : MonoBehaviour
         {
             if (_instance == null)
             {
-                var obj = new GameObject("FritzVisionUnity");
+                var obj = new GameObject("FritzPoseUnity");
                 _instance = obj.AddComponent<FritzVisionUnity>();
             }
 
@@ -128,24 +132,42 @@ public class FritzVisionUnity : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
-        Configure();
-    }
+        FritzPoseManager.Configure();
 
-    private void Start()
-    {
-        StartCamera();
     }
 
     private void Update()
     {
-        var poses = GetLatestPose();
-        //foreach (FritzPose pose in poses)
-        //{
-        //    foreach (Keypoint keypoint in pose.keypoints)
-        //    {
-        //        Debug.Log(keypoint.position);
-        //    }
-        //}
+        if (Time.frameCount % 10 != 0) { return;  }
+
+        var cameraParams = new XRCameraParams
+        {
+            zNear = m_Cam.nearClipPlane,
+            zFar = m_Cam.farClipPlane,
+            screenWidth = Screen.width,
+            screenHeight = Screen.height,
+            screenOrientation = Screen.orientation
+        };
         
+        XRCameraFrame frame;
+        if (!cameraManager.subsystem.TryGetLatestFrame(cameraParams, out frame))
+        {
+            return;
+        }
+
+        var poses = FritzPoseManager.ProcessPose(frame.nativePtr);
+        foreach (FritzPose pose in poses)
+        {
+            foreach (Keypoint keypoint in pose.keypoints)
+            {
+                Debug.Log(keypoint.position);
+            }
+        }
+
+    }
+
+    unsafe void OnCameraFrameReceived(ARCameraFrameEventArgs eventArgs)
+    {
+        Debug.Log("Camera frame recieved");
     }
 }
--- Assets/Plugins/iOS/FritzVisionUnity/download_fritz_frameworks.sh
+++ Assets/Plugins/iOS/FritzVisionUnity/download_fritz_frameworks.sh
@@ -2,7 +2,7 @@
 
 # Download sdk to Frameworks folder.
 
-SDK_VERSION=3.5.6
+SDK_VERSION=3.7.0-beta.1
 DEST=Frameworks/
 
 
@@ -10,7 +10,10 @@ function downloadFramework() {
     FRAMEWORK=$1
 
     echo "Downloading Framework $FRAMEWORK"
+    rm ${FRAMEWORK}.zip
+    rm -r $DEST/*
     wget "https://github.com/fritzlabs/swift-framework/releases/download/${SDK_VERSION}/${FRAMEWORK}.zip"
+
     unzip -o ${FRAMEWORK}.zip -d tmp
     mv tmp/Frameworks/* $DEST
     rm ${FRAMEWORK}.zip
