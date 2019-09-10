97df5c0cad9831f7b349dd5cf8eae470f0d0cf48
Chris Kelly <ghop02@gmail.com>
2019-09-10 18:40:02 -0400

    Adding bird perch demo

---
+++ FritzUnityBirdPerchPoseDemo/Assets/Plugins/iOS/FritzVisionUnity/Editor/FritzInfoPostProcess.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using UnityEditor.Build.Player;
+
+
+using System.IO;
+using System.Linq;
+
+public static class FritzInfoPostProcess
+{
+
+    [PostProcessBuild]
+    public static void OnPostProcessBuild(BuildTarget buildTarget, string buildPath)
+    {
+        if (buildTarget != BuildTarget.iOS)
+        {
+            return;
+        }
+
+        string pluginPath = "Plugins/iOS/FritzVisionUnity/Source/";
+        string sourceFolder = Path.Combine(Application.dataPath, pluginPath);
+        string libraryPath = "Libraries/Plugins/iOS/FritzVisionUnity/Source/";
+        string plistPath = Path.Combine(sourceFolder, "Fritz-Info.plist");
+        string xcodePath = Path.Combine(buildPath, libraryPath, "Fritz-Info.plist");
+
+        PlistDocument plist = new PlistDocument();
+        plist.ReadFromFile(plistPath);
+
+        // TODO: Pull out API Key specification here, until then, set configuration in Source/Fritz-Info.plist file.
+
+        //plist.root.SetString("apiKey", "YOUR API KEY HERE");
+        //plist.root.SetString("apiUrl", "https://api.fritz.ai/sdk/v1");
+        //plist.root.SetString("namespace", "production");
+
+        File.WriteAllText(xcodePath, plist.WriteToString());
+
+        var projPath = buildPath + "/Unity-Iphone.xcodeproj/project.pbxproj";
+        var proj = new PBXProject();
+        proj.ReadFromFile(projPath);
+
+        var targetGuid = proj.TargetGuidByName(PBXProject.GetUnityTargetName());
+
+        string plistGuid = proj.AddFile(xcodePath, Path.Combine(libraryPath, "Fritz-Info.plist"));
+        proj.AddFileToBuild(targetGuid, plistGuid);
+
+        proj.WriteToFile(projPath);
+
+
+        // Update Info with Camera usage description
+        string infoPath = Path.Combine(buildPath, "Info.plist");
+        PlistDocument infoPlist = new PlistDocument();
+
+        infoPlist.ReadFromFile(infoPath);
+        infoPlist.root.SetString("NSCameraUsageDescription", "For ML Camera Usage");
+        File.WriteAllText(infoPath, infoPlist.WriteToString());
+
+    }
+
+}
---
+++ FritzUnityBirdPerchPoseDemo/Assets/Plugins/iOS/FritzVisionUnity/download_fritz_frameworks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Download sdk to Frameworks folder.
+
+SDK_VERSION=4.0.0-beta.2
+DEST=Frameworks/
+
+
+function downloadFramework() {
+    FRAMEWORK=$1
+
+    echo "Downloading Framework $FRAMEWORK"
+    rm ${FRAMEWORK}.zip
+    wget "https://github.com/fritzlabs/swift-framework/releases/download/${SDK_VERSION}/${FRAMEWORK}.zip"
+
+    unzip -o ${FRAMEWORK}.zip -d tmp
+    cp -R tmp/Frameworks/* $DEST
+    rm ${FRAMEWORK}.zip
+}
+
+mkdir tmp/
+downloadFramework FritzBase
+downloadFramework FritzVisionPoseModel
+rm -rf tmp/
